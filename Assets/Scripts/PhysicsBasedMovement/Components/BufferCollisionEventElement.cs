8215588d40d721df84c4513c1edbc186d243fb21
Ammon <s0566075@htw-berlin.de>
2020-06-05 13:57:32 +0200

    # 19 working collision system for entities

---
+++ Assets/Scripts/PhysicsBasedMovement/Components/BufferCollisionEventElement.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+public struct BufferCollisionEventElement : IBufferElementData
+{
+    public Entity Entity;
+    public float3 Normal;
+    public bool HasCollisionDetails;
+    public float3 AverageContactPointPosition;
+    public float EstimatedImpulse;
+    public PhysicsEventState State;
+    public bool isStale;
+}
+
+public struct CollisionEventsReceiverProperties : IComponentData
+{
+    public bool UsesCollisionDetails;
+}
